f993a9ad2cc1a3cba8740102c7e44ca7adacb213
nd2204 <dn200422@gmail.com>
2025-11-10 15:15:09 +0700

    feat(fe): add device tables

--- siams-fe/src/components/cluster/ClusterDeviceView.tsx
+++ siams-fe/src/components/cluster/ClusterDeviceView.tsx
@@ -1,13 +1,19 @@
 import type { Cluster } from "@/types/device";
-import { Card, CardContent } from "../ui/card";
+import { Card } from "../ui/card";
 import ClusterDeviceEmpty from "./ClusterDeviceEmpty";
 import { useAuth } from "@/hooks/use-auth"
 import { useCallback } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { QUERIES } from "@/hooks/queries/query-keys";
+import { useDeviceByCluster } from "@/hooks/queries/use-device-by-cluster";
+import { ClusterDeviceDataTable } from "./ClusterDeviceDataTable";
+import { clusterDeviceColumns } from "./ClusterDeviceColumn";
+import { Skeleton } from "../ui/skeleton";
+import { Badge } from "../ui/badge";
 
 export default function ClusterDeviceView({ cluster }: { cluster: Cluster }) {
   const { activeOrg } = useAuth()
+  const { data: devices, isPending } = useDeviceByCluster(cluster.id);
   const queryClient = useQueryClient();
 
   const onRefresh = useCallback(() => {
@@ -16,16 +22,26 @@ export default function ClusterDeviceView({ cluster }: { cluster: Cluster }) {
     })
   }, [])
 
+
+  if (isPending) {
+    return (
+      <Skeleton />
+    )
+  }
+
   return (
     <>
-      {(cluster.devices && cluster.devices.pagination.total > 0) ? (
+      {(devices && devices.pagination.total > 0) ? (
         <div className="flex flex-1">
-          <Card className="@container/card flex-1 overflow-hidden pt-2 pb-0 gap-0">
-            <CardContent className="px-2">
-            </CardContent>
-          </Card>
-          <Card className="@container/card flex flex-1 overflow-hidden p-0 m-2">
-          </Card>
+          <div className="@container/card flex-1 overflow-hidden pt-2 pb-0 gap-0">
+            <div className="flex justify-between pb-4">
+              <div className="font-semibold">Devices in this Cluster</div>
+              <Badge variant="outline">
+                {devices.pagination.total} devices
+              </Badge>
+            </div>
+            <ClusterDeviceDataTable columns={clusterDeviceColumns} data={devices.data} />
+          </div>
         </div>
       ) : (
         <Card className="@container/card flex flex-1 bg-background overflow-hidden pt-2 pb-0 gap-0 border-dashed">
