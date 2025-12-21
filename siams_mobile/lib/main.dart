8b7028a9e8d776766fa571a64c33906018967c1c
nd2204 <dn200422@gmail.com>
2025-12-21 21:50:28 +0700

    add flutter project

---
+++ siams_mobile/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
